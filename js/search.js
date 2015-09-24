ad35071d3aa19b8e1edc701cf68c9edfd8954e4a
Yuan Tao <yt11551n@pace.edu>
2015-09-24 17:15:20 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -6,13 +6,15 @@ $("#menu_signup").click(function(e){
 	 e.preventDefault();
 	 $("#signup").modal();
 });
+
+$("#menu_forgotpwd").click(function(e){
+	 e.preventDefault();
+	 $("#forgotpwd").modal();
+});
+
 $("#browse").click(function(e) {
 	e.preventDefault();
 	$("#search").toggleClass("hide");
 	$("#search").find("input").focus();
 	$(".list-inline.intro-social-buttons").addClass("hide");
-})
-$("#search").find("input").focusout(function(){
-	$("#search").toggleClass("hide");
-	$(".list-inline.intro-social-buttons").removeClass("hide");
 })
